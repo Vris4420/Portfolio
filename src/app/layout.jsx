6a90d0e5a3568710100f5635bad5d9a1f8683890
Vrishank Sharma <sharma08462@gmail.com>
2024-05-29 12:08:55 +0530

    add StairAnimation

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Variable, Weight } from "lucide-react";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const JetbrainsMono = JetBrains_Mono({ 
  subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={JetbrainsMono.variable}>
         <Header />
+        <StairTransition/>
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
