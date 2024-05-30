e1c8c8c8577c3e604f0ab8ff0010a1ef34169fa8
Vrishank Sharma <sharma08462@gmail.com>
2024-05-30 23:56:53 +0530

    fixes

--- src/components/StairTransition.jsx
+++ src/components/StairTransition.jsx
@@ -7,21 +7,6 @@ import Stairs from "./Stairs";
 const StairTransition = () => {
   const pathname = usePathname();
 
-  // return (
-  //   <AnimatePresence mode="wait">
-  //     <motion.div
-  //       key={pathname}
-  //       initial={{ opacity: 1 }}
-  //       animate={{ opacity: 1 }}
-  //       exit={{ opacity: 0 }}
-  //       transition={{ duration: 0.4 }}
-  //     >
-  //       <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none flex">
-  //         <Stairs />
-  //       </div>
-  //     </motion.div>
-  //   </AnimatePresence>
-  // );
 
   return (
     <>
