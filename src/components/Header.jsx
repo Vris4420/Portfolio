912e849f2bb3a6d0193db83ce04be62502a5f381
Vrishank Sharma <sharma08462@gmail.com>
2024-05-31 00:41:49 +0530

    MobileNav and transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+    <header className="py-8 xl:py-12 text-white bg-pink">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className=" text-4xl font-semibold">
@@ -21,7 +22,9 @@ const Header = () => {
         </div>
 
         {/* Mobile Nav */}
-        <div className="xl:hidden">Mobile Nav</div>
+        <div className="xl:hidden">
+          <MobileNav/>
+        </div>
 
       </div>
     </header>
--- src/components/StairTransition.jsx
+++ src/components/StairTransition.jsx
@@ -13,8 +13,16 @@ const StairTransition = () => {
       <AnimatePresence mode="wait">
         <div key={pathname}>
           <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none flex">
-            <Stairs/>
+            <Stairs />
           </div>
+
+          {/* <motion.div className="h-screen w-screen fixed top-0 bg-primary pointer-events-none"
+            initial={{opacity:1}}
+            animate={{
+              opacity:0,
+              transition:{delay:1, duration:0.4, ease:"easeInOut"}
+            }}
+          /> */}
         </div>
       </AnimatePresence>
     </>
