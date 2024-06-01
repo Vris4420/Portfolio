5ef931f638412daba7330b41b1cd2ecc2a3501e8
Vrishank Sharma <sharma08462@gmail.com>
2024-06-01 11:12:09 +0530

    add home page and change global.css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {FiDownload} from 'react-icons/fi'
 
 export default function Home() {
   return (
-  <main>
-    homepage
-  </main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 ">
+          {/* text */}
+          <div className="text-center xl:text-left ">
+            <span>Software Developer</span>
+            <h1 className="h1">
+              Hello I'm <br /> <span>Luke Coleman</span>
+            </h1>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
 }
 
