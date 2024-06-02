ac12f8f0adac2d004ff47b7e433e3fb4cfbb42d2
Vrishank Sharma <sharma08462@gmail.com>
2024-06-02 12:14:47 +0530

    add style to homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,6 +17,19 @@ export default function Home() {
               in various programming languages and technologies
             </p>
             {/* Buttons and socials */}
+            <div className="flex  flex-col xl:flex-row items-center gap-8" >
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials/>
+              </div>
+            </div>
           </div>
           <div>photo</div>
         </div>
