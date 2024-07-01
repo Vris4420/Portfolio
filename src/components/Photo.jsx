9ac3939efdf2e0d30034aecc35560cb4c9612b91
Vrishank Sharma <sharma08462@gmail.com>
2024-07-01 14:01:13 +0530

    updates

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -19,7 +19,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/profilePic.png"
+            src="/profilePic2.png"
             priority
             quality={100}
             layout="fill"
